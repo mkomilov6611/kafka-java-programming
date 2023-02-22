c0d66903882f69b3fe6e2b125a6514dc69b54484
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2023-02-22 22:41:47 +0500

    Producer with callback and key

--- KafkaBasics/src/main/java/io/conduktor/demos/kafka/ProducerWithCallback.java
+++ KafkaBasics/src/main/java/io/conduktor/demos/kafka/ProducerWithCallback.java
@@ -1,18 +1,20 @@
 package io.conduktor.demos.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class Producer {
-    private static final Logger log = LoggerFactory.getLogger(ProducerWithKeys.class.getSimpleName());
+public class ProducerWithCallback {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.info("Producer with Keys");
+        log.info("Producer");
 
 //        Create Producer Properties
         Properties properties = new Properties();
@@ -27,13 +29,43 @@ public class Producer {
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
+//        Not recommended on production, just for demo
+        properties.setProperty("batch.size", "400");
+
 //        Create the Producer
         KafkaProducer <String, String> producer = new KafkaProducer<>(properties);
 
-//        Create Producer Record to send data
-        ProducerRecord <String, String> producerRecord = new ProducerRecord<>("demo_topic", "HelloWorld!");
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < 50; i++) {
+                //        Create Producer Record to send data
+                ProducerRecord <String, String> producerRecord = new ProducerRecord<>("demo_topic", "HelloWorld! " + i);
 
-        producer.send(producerRecord);
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        if(exception == null) {
+                            log.info(
+                                    "\nReceived metadata" +
+                                            "\nTopic: " + metadata.topic() +
+                                            "\nPartition: " + metadata.partition() +
+                                            "\nOffset: " + metadata.offset() +
+                                            "\nTimestamp: " + metadata.timestamp()
+                            );
+                        } else {
+                            log.error("Error while producing: ", exception);
+                        }
+                    }
+                });
+            }
+
+//            sleep between
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 
 //        tell the producer to send all the data and block until done --synchronous
         producer.flush();
--- KafkaBasics/src/main/java/io/conduktor/demos/kafka/ProducerWithKeys.java
+++ KafkaBasics/src/main/java/io/conduktor/demos/kafka/ProducerWithKeys.java
@@ -1,18 +1,20 @@
 package io.conduktor.demos.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class Producer {
-    private static final Logger log = LoggerFactory.getLogger(ProducerWithKeys.class.getSimpleName());
+public class ProducerWithKeys {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.info("Producer with Keys");
+        log.info("Producer");
 
 //        Create Producer Properties
         Properties properties = new Properties();
@@ -27,13 +29,38 @@ public class Producer {
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
+
 //        Create the Producer
-        KafkaProducer <String, String> producer = new KafkaProducer<>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-//        Create Producer Record to send data
-        ProducerRecord <String, String> producerRecord = new ProducerRecord<>("demo_topic", "HelloWorld!");
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 10; i++) {
+                String topic = "demo_topic";
+                String key = "key " + i;
+                String value = "HelloWorld! " + i;
+                //        Create Producer Record to send data
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, value );
 
-        producer.send(producerRecord);
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        if (exception == null) {
+                            log.info("Key: " + key + " | Partition: " + metadata.partition());
+                        } else {
+                            log.error("Error while producing: ", exception);
+                        }
+                    }
+                });
+            }
+
+            try {
+                Thread.sleep(2000);
+                log.info("Slept for 2 sec");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 
 //        tell the producer to send all the data and block until done --synchronous
         producer.flush();
